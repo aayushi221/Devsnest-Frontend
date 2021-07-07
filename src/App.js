5bc3a9ecf639d1fab5ddf9cc3cc5d86306301798
Aayushi Pandey <aayushipandey221@gmail.com>
2021-07-07 18:34:20 +0530

    day 16 THA done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+  
+import {Component} from 'react'
+// import ChessBoard from "./chessBoard/ChessBoard";
+import MemeCard from "./memeCard/memeCard";
+
+class App extends Component{
+  render(){
+    return (
+        <>
+            <MemeCard/>
+            {/* <ChessBoard /> */}
+        </>
+    )
+  }
+}
+
+export default App;
