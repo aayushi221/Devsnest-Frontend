df7c6a6a7888c883a38853ecf4dc3529ed184105
aayushi221 <aayushipandey221@gmail.com>
2021-07-26 20:56:36 +0530

    error in mapping

--- dontreact/src/day22/Meme.jsx
+++ dontreact/src/day22/Meme.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Meme = ({meme, setMeme}) => {
     // console.log(meme);
+    const [form, setForm] = useState({
+        template_id: meme.id,
+        username:"AayushiPandey",
+        password:"7z3QXMN2u@AZeKU",
+        boxes: [],
+    
+    });
+    const generateMeme = () => {
+        console.log(form);
+    };
  return ( 
      <div className= "meme">
          <img src = {meme.url} alt=""/>
          <div>
          {
              [...Array(meme.box_count)].map((_, index) => (
-                <input type="text" placeholder = {`Meme Caption ${index+1} `} />
+                <input key={index} type="text" placeholder = {`Meme Caption ${index+1} `}  onChange = {(e) => {
+                    const newBoxes = form.boxes;
+                    newBoxes[index] = {text: e.target.value};
+                    setForm({...form, boxes: newBoxes});
+                } }/>
              ))
         }
         </div>
         <div>
-            <button>
+            <button onClick = { generateMeme }>
                 Generate Meme
             </button>
             <button onClick= {() => {
