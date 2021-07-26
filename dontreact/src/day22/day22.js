56bef4985a374c3e5bc4c8d16fc945b709fe3cc2
aayushi221 <aayushipandey221@gmail.com>
2021-07-26 13:45:45 +0530

    day 22

--- dontreact/src/day22/day22.js
+++ dontreact/src/day22/day22.js
@@ -1,16 +1,31 @@
 
 import React, {useState, useEffect} from 'react';
+
 import './day22.css';
 
 
 function Day22(){
     const [templates, setTemplates] = useState([]);
+    const [meme, setMeme]= useState(null);
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
+        .then((res) => res.json())
         .then((data) => {
-            console.log(data);
-        })
-    }, [])
+            setTemplates(data.data.memes);
+            console.log(data.data.memes);
+        });
+    }, []);
+    return (
+        <div className="Appy ">
+            <h1>Meme Generator</h1>
+            {templates.map(template =>(
+               <div key={template.id} className="template">
+                   
+                   <div style = {{backgroundImage: `url(${template.url})`}} className="image">
+                   </div>
+               </div> 
+            ))}
+        </div>
+    )
 }
 export default Day22; 
