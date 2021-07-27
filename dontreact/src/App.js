7853af3d269b46d74315068bb4a1b077746c4506
aayushi221 <aayushipandey221@gmail.com>
2021-07-27 11:37:46 +0530

    💡 comments added in app.js

--- dontreact/src/App.js
+++ dontreact/src/App.js
@@ -5,6 +5,8 @@ import Day19 from "./day19/day19";
 import Day20 from "./day20/day20";
 import Day21 from "./day21/day21";
 import Day22 from "./day22/day22";
+import Day23 from "./day23/day23";
+
 
 
 
@@ -13,22 +15,31 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/day17">
-          <Day17 />
+          <Day17 /> 
+          {/* calorie counter */}
         </Route>
         <Route exact path="/day18">
           <Day18 />
+          {/* chessboard */}
         </Route>
         <Route exact path="/day19">
           <Day19 />
+          {/* counter */}
         </Route>
         <Route exact path="/day20">
           <Day20 />
+          {/* calorie counter ft. usestate */}
         </Route>
         <Route exact path="/day21">
           <Day21 />
+          {/* calorie counter X useState (philosophical sessions[CRUD]) */}
         </Route>
         <Route exact path="/day22">
           <Day22 />
+          {/* meme generator */}
+        </Route>
+        <Route exact path="/day23">
+          <Day23 />
         </Route>
       </Switch>
     </Router>
---
+++ dontreact/src/day23/day23.jsx
@@ -0,0 +1,5 @@
+function Day23() {
+    return( <h1> hi</h1>)
+}
+
+export  default Day23;
